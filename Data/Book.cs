cada41c465b6aea7f7c96d21aca5a9556daeced2
ShubhamDev021 <shubhat021@gmail.com>
2025-02-07 13:55:29 +0530

    Reverting aapplied migrations: (Method 2) - Part 2 - commented out Adding column 3 code and then created that migration using dotnet ef migrations add RemovedColumn3 and then applying the migrations using dotnet ef database update

--- Data/Book.cs
+++ Data/Book.cs
@@ -10,7 +10,6 @@
         public DateTime CreatedOn { get; set; }
         public int Column1 { get; set; }
         public int Column2 { get; set; }
-        public int Column3 { get; set; }
         public int Column4 { get; set; }
     }
 }
---
+++ Migrations/20250207082141_RemovedColumn3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCoreProject1.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedColumn3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Column3",
+                table: "Books");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Column3",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
