24448a6d82228721936eb06b25568989b2727f48
ShubhamDev021 <shubhat021@gmail.com>
2025-02-07 17:40:19 +0530

    Inserting master table data using migration i.e. dotnet ef migrations add InsertingMasterTableData, dotnet ef database update

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,6 +9,23 @@ namespace EntityFrameworkCoreProject1.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Currency>().HasData(
+                new Currency() { Id = 1, Title = "INR", Description = "Indian INR" },
+                new Currency() { Id = 2, Title = "USD", Description = "USD($)" },
+                new Currency() { Id = 3, Title = "Euro", Description = "Euro" },
+                new Currency() { Id = 4, Title = "Dinar", Description = "Dinar" }
+            );
+
+            modelBuilder.Entity<Language>().HasData(
+                new Language() { Id = 1, Title= "Hindi", Description = "Hindi" },
+                new Language() { Id = 2, Title= "Punjabi", Description = "Punjabi" },
+                new Language() { Id = 3, Title= "Tamil", Description = "Tamil" },
+                new Language() { Id = 4, Title= "Urdu", Description ="Urdu" }
+            );
+        }
+
         //To inform DB Context to use the table in Entity Framework Core, use DbSet property as below
         public DbSet<Book> Books { get; set; }
         public DbSet<Language> Languages { get; set; }
---
+++ Migrations/20250207120755_InsertingMasterTableData.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityFrameworkCoreProject1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertingMasterTableData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Currencies",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Indian INR", "INR" },
+                    { 2, "USD($)", "USD" },
+                    { 3, "Euro", "Euro" },
+                    { 4, "Dinar", "Dinar" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Hindi", "Hindi" },
+                    { 2, "Punjabi", "Punjabi" },
+                    { 3, "Tamil", "Tamil" },
+                    { 4, "Urdu", "Urdu" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Currencies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
