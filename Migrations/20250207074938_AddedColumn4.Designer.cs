c86db53dd4e028dadddcadd6bcc4bc456f19099a
ShubhamDev021 <shubhat021@gmail.com>
2025-02-07 14:22:10 +0530

    Revert "Reverting unapplied migrations (Part 3): removed the new migration i.e. dotnet ef migrations remove"
    This reverts commit cae96e83053646bd58b4bdc5201576a3186b638e.

--- Migrations/20250207074938_AddedColumn4.Designer.cs
+++ Migrations/20250207074938_AddedColumn4.Designer.cs
@@ -4,6 +4,7 @@ using EntityFrameworkCoreProject1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EntityFrameworkCoreProject1.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250207074938_AddedColumn4")]
+    partial class AddedColumn4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
