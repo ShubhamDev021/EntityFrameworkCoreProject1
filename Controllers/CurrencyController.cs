48865ee26e24f276439a5809260d1bd81257563e
ShubhamDev021 <shubhat021@gmail.com>
2025-02-07 18:05:11 +0530

    Adding currency controller to view all the currencies by API

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCoreProject1.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCoreProject1.Controllers
+{
+    [Route("api/currencies")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CurrencyController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetAllCurrencies()
+        {
+            //method 1
+            //var result = _appDbContext.Currencies.ToList();
+
+            //method 2
+            var result = (from currencies in _appDbContext.Currencies
+                          select currencies).ToList();
+            return Ok(result);
+        }
+    }
+}
