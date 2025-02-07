61a23d11f0ad823a731b4ee6a164411e3dbe2bdc
ShubhamDev021 <shubhat021@gmail.com>
2025-02-07 14:21:35 +0530

    Revert "Removed earlier created columns"
    This reverts commit 1dd892cbcd4af5bc9257e11b414d79f0b537fa54.

--- Data/Book.cs
+++ Data/Book.cs
@@ -8,5 +8,8 @@
         public int NumberOfPages { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
+        public int Column1 { get; set; }
+        public int Column2 { get; set; }
+        public int Column4 { get; set; }
     }
 }
